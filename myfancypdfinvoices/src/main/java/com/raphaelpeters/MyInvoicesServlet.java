b45e5b419acd395135b841d92fe9a83be117be02
RAPHAEL PETERS <RKCPETERS@gmail.com>
2025-03-09 09:44:20 +0000

    extend servlet to respond with json

---
+++ myfancypdfinvoices/src/main/java/com/raphaelpeters/MyInvoicesServlet.java
@@ -0,0 +1,31 @@
+package com.raphaelpeters;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyInvoicesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getRequestURI().equalsIgnoreCase("/")) {
+            response.setContentType("text/html; charset=UTF-8");
+            response.getWriter().print(
+                    "<html>\n" +
+                            "<body>\n" +
+                            "<h1>Hello World</h1>\n" +
+                            "<p>Raphael has created a embedded Tomcat, HTML Page!</p>\n" +
+                            "</body>\n" +
+                            "</html>");
+        } else if (request.getRequestURI().equalsIgnoreCase("/invoices")) {
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().print("[empty json]");
+        }
+
+
+    }
+
+
+}
