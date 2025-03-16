c09c289d0228faf659f2714ce13c47325368d55b
RAPHAEL PETERS <RKCPETERS@gmail.com>
2025-03-16 12:23:22 +0000

    Update scope of user bean to prototype for better thread safety

--- myfancypdfinvoices/src/main/java/com/raphaelpeters/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ myfancypdfinvoices/src/main/java/com/raphaelpeters/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -3,13 +3,16 @@ package com.raphaelpeters.myfancypdfinvoices.context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raphaelpeters.myfancypdfinvoices.service.InvoiceService;
 import com.raphaelpeters.myfancypdfinvoices.service.UserService;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UserService userService() {
         return new UserService();
     }
